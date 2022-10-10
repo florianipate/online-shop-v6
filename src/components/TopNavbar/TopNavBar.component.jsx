11059932aad6d278b567e9c8737f76916f701d47
Florian Ipate <florian.ipate@gmail.com>
2022-10-10 20:13:28 +0100

    update

--- src/components/TopNavbar/TopNavBar.component.jsx
+++ src/components/TopNavbar/TopNavBar.component.jsx
@@ -2,23 +2,62 @@ import { Link } from "react-router-dom";
 import './TopNavbar.style.css';
 const TopNavbar = () =>{
     return(
-        <nav className="bg-white">
-            <div className="flex justify-spacebetween">
+        <nav>
+            <div className="top-nav-wrapper justify-spacebetween">
                 <div className="logo-wrapper">Logo</div>
                 <ul className="nav-links-wrapper">
-                    <li>
+                    <li className="nav-link-item">
                         <Link>Link 1</Link>
                     </li>
-                    <li>
+                    <li className="nav-link-item">
+
                         <Link>Link 2</Link>
                     </li>
-                    <li>
-                        <Link>Link 3</Link>
+                    <li className="nav-link-item has-dropdown">
+                        <Link>
+                            Link 3
+                            <span className="arrow down"></span>
+                        </Link>
+                        
+                        <ul className="submenu">
+                            <li className="nav-link-item">
+                                <Link>Submenu Link 1</Link>
+                            </li>
+                            <li className="nav-link-item">
+                                <Link>Submenu Link 2</Link>
+                            </li>
+                            <li className="nav-link-item has-dropdown">
+                                <Link>Submenu Link 3</Link>
+                                <span className="arrow"></span>
+                                <ul className="submenu1">
+                                    <li className="nav-link-item">
+                                        <Link>Inner Submenu Link 1</Link>
+                                    </li>
+                                    <li className="nav-link-item">
+                                        <Link>Inner Submenu Link 2</Link>
+                                    </li>
+                                    <li className="nav-link-item">
+                                        <Link>Inner Submenu Link 3</Link>
+                                    </li>
+                                    <li className="nav-link-item">
+                                        <Link>inner Submenu Link 4</Link>
+                                    </li>
+                                </ul>
+
+
+
+
+                            </li>
+                            <li className="nav-link-item">
+                                <Link>Submenu Link 4</Link>
+                            </li>
+                        </ul>
+                        
                     </li>
-                    <li>
+                    <li className="nav-link-item">
                         <Link>Link 4</Link>
                     </li>
-                    <li>
+                    <li className="nav-link-item">
                         <Link>Link 5</Link>
                     </li>
                 </ul>
